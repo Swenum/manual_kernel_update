e332e1b1c0483c492d3f6c7fa77c982ce2e904db
uladzimirschuka <uladzimir.schuka@moneyman.ru>
2020-02-03 18:01:33 +0300

    Add  download kernel src make and install modules

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,20 @@
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl  restart sshd
 yum install -y epel-release
+uname -r > /root/kernel.txt
 yum update       
 yum install -y patch xz-utils kernel-devel kernel-headers gcc make dkms perl bzip2 ncurses-devel  bc bison flex elfutils-libelf-devel openssl-devel grub2 wget curl iotop mc htop screen  
-cd /usr/src/ && wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.5.tar.xz && unxz -v linux-5.5.tar.xz
+cd /usr/src/ && wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.5.tar.xz && tar -xvf linux-5.5.tar.xz
 cd /usr/src/  && wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/patch-5.5.xz && unxz patch-5.5.xz
 yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 yum --enablerepo elrepo-kernel install kernel-ml -y
+grub2-mkconfig -o /boot/grub2/grub.cfg
+grub2-set-default 0
+reboot
+uname -r >> /root/kernel.txt
+cd /usr/src/linux-5.5 && make defconfig && make -j5
+make modules_install install
+ls -l /boot/ | tail -n 4
+
 	     
 
