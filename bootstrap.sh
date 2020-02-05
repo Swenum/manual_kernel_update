4d9c0a1baaace913f6dabe064a7a588b0519d556
uladzimirschuka <uladzimir.schuka@moneyman.ru>
2020-02-05 13:43:51 +0300

    Assemble kernel

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,14 @@ yum install -y epel-release
 uname -r > /root/kernel.txt
 yum update       
 yum install -y patch xz-utils kernel-devel kernel-headers gcc make dkms perl bzip2 ncurses-devel  bc bison flex elfutils-libelf-devel openssl-devel grub2 wget curl iotop mc htop screen  
-cd /usr/src/ && wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.5.tar.xz && tar -xvf linux-5.5.tar.xz
-cd /usr/src/  && wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/patch-5.5.xz && unxz patch-5.5.xz
+cd /usr/src/ && wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.5.2.tar.gz && tar -xvf linux-5.5.2.tar.gz
 yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 yum --enablerepo elrepo-kernel install kernel-ml -y
 grub2-mkconfig -o /boot/grub2/grub.cfg
 grub2-set-default 0
-reboot
-uname -r >> /root/kernel.txt
-cd /usr/src/linux-5.5 && make defconfig && make -j5
-make modules_install install
-ls -l /boot/ | tail -n 4
-
+cp /boot/config-`uname -r` /usr/src/linux-5.5.2/.config 
+cd /usr/src/linux-5.5.2 && make olddefconfig &&  make -j5 && make modules_install install
+#ls -l /boot/ | tail -n 4
+grub2-set-default 0
 	     
 
---
+++ readme.md
@@ -0,0 +1,18 @@
+
+```bash
+bash VBoxLinuxAdditions.run
+Verifying archive integrity... All good.
+Uncompressing VirtualBox 6.0.16 Guest Additions for Linux........
+VirtualBox Guest Additions installer
+Removing installed version 6.0.16 of VirtualBox Guest Additions...
+Copying additional installer modules ...
+Installing additional modules ...
+VirtualBox Guest Additions: Starting.
+VirtualBox Guest Additions: Building the VirtualBox Guest Additions kernel
+modules.  This may take a while.
+VirtualBox Guest Additions: To build modules for other installed kernels, run
+VirtualBox Guest Additions:   /sbin/rcvboxadd quicksetup <version>
+VirtualBox Guest Additions: or
+VirtualBox Guest Additions:   /sbin/rcvboxadd quicksetup all
+VirtualBox Guest Additions: Building the modules for kernel 5.5.2.
+```
